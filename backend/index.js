21677b57ec636d43a3b70b278d097457cf0b0e8e
Nithin0620 <nithin30302@gmail.com>
2025-07-19 15:32:41 +0530

    Completed

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const path = require("path")
 
 const anouncementApi = require("./routes/Anouncements.routes")
 
@@ -9,11 +10,23 @@ const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use("/api/announcements",anouncementApi);
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
 
 
 app.listen(PORT,()=>{
    console.log(`server started successfully and is running in Port-${PORT}`)
 })
+
+app.get("/" , (req,res)=>{
+  res.send(`<h1> This is homepage, response from server hance the server is up and running <h1/>`)
+})
+
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
--- frontend/src/store/Announcement.jsx
+++ frontend/src/store/Announcement.jsx
@@ -2,7 +2,9 @@ import { create } from "zustand";
 import axios from "axios";
 
 
-const BASE_URL = "http://localhost:4000/api/announcements";
+const BASE_URL = process.env.NODE_ENV === "development"
+  ? "http://localhost:4000/api"
+  : "/api/announcements";
 
 export const useAnnouncementStore = create((set, get) => ({
   isAuthenticated: false,
